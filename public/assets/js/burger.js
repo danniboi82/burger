422aeee68f6cf567bb4f6e011874ce293d0be493
Dan Oh <danoh82@gmail.com>
2017-12-07 23:29:42 -0800

    WHAT AM I DOING WRONG??

---
+++ public/assets/js/burger.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEat = $(this).data("neweat");
+  
+      var newEat = {
+        eaten : newEat
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEat
+      }).then(
+        function() {
+          console.log("changed eat to", newEat);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-burger-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        eaten: 0,
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new hamburger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
